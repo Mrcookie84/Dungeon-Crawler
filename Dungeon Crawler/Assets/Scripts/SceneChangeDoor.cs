e6999d300f61a99806a63de25a22b01b50f24d4f
Mrcookie <hautcoeur.corentin@gmail.com>
2025-03-18 14:23:18 +0100

    Teleporteur + puzzle + Porte final

---
+++ Dungeon Crawler/Assets/Scripts/SceneChangeDoor.cs
@@ -0,0 +1,75 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using DG.Tweening;
+
+public class SceneChangeDoor : MonoBehaviour
+{
+    [Header("Temporary solution to unlock :")] 
+    [SerializeField] public GameObject solution;
+    
+    
+    [Header("Door Sprite :")]
+    [SerializeField] public GameObject doorClosed;
+    [SerializeField] public GameObject doorOpened;
+    
+    [Header("Others :")]
+    [SerializeField] public GameObject interactBubble;
+    [SerializeField] public BoxCollider2D triggerBox;
+    [SerializeField] public GameObject mapUI;
+
+    [Header("Scene to go :")]
+    [SerializeField] public int sceneNumber = 0;
+
+
+    private void Start()
+    {
+        doorOpened.SetActive(false);
+        interactBubble.SetActive(false);
+        triggerBox.enabled = false;
+        doorClosed.SetActive(true);
+    }
+
+    private void Update()
+    {
+
+        if (solution.activeSelf == false)
+        {
+            doorClosed.SetActive(false);
+            doorOpened.SetActive(true);
+            triggerBox.enabled = true;
+        }
+        else
+        {
+            doorClosed.SetActive(true);
+            doorOpened.SetActive(false);
+            triggerBox.enabled = false;
+        }
+
+        if (interactBubble.activeSelf && doorOpened.activeSelf && Input.GetKeyDown(KeyCode.E) && solution.activeSelf == false)
+        {
+
+            
+
+        }
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player") && triggerBox.enabled == true)
+        {
+            interactBubble.SetActive(true);
+        }
+    }
+    
+    void OnTriggerExit2D(Collider2D other )
+    {
+        if (other.gameObject.CompareTag("Player") && triggerBox.enabled == true)
+        {
+            interactBubble.SetActive(false);
+        }
+    }
+}
