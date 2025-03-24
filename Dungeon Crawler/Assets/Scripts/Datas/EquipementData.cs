c73ec018e47044afc4a28c6165f345419e1444b4
Piette Quentin <q.piette@rubika-edu.com>
2025-03-24 16:13:41 +0100

    test combat pas fonctionnel

--- Dungeon Crawler/Assets/Scripts/Datas/EquipementData.cs
+++ Dungeon Crawler/Assets/Scripts/Datas/EquipementData.cs
@@ -2,7 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 [CreateAssetMenu(fileName = "Equipement", menuName = "ScriptableObject/Equipement")]
-public class EquipementData : Runes
+public class EquipementData : ScriptableObject 
 {
     public Sprite imageEquipment;
+    [SerializeField]
+    private List<Runes> listOfRunes = new List<Runes>();
 }
--- Dungeon Crawler/Assets/Scripts/Datas/Runes.cs
+++ Dungeon Crawler/Assets/Scripts/Datas/Runes.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+[CreateAssetMenu(fileName = "Data", menuName = "ScriptableObject/Runes")]
 public class Runes : Spells
 {
     public Sprite imageRunes;
