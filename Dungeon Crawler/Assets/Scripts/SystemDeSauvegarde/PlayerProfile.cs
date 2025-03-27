0e6f2daa9d7da83db3333fe4d421327c04628587
Mrcookie <hautcoeur.corentin@gmail.com>
2025-03-27 14:49:50 +0100

    Combat trigger + map UI + fusion des deux dossier prefab
    Mapplus zone d'explo
    
    UI + PlayerXp/PlayerLvl + System de sauvegarde
    
    datads

---
+++ Dungeon Crawler/Assets/Scripts/SystemDeSauvegarde/PlayerProfile.cs
@@ -0,0 +1,12 @@
+namespace SystemDeSauvegarde
+{
+    [System.Serializable]
+    public class PlayerProfile
+    {
+
+        public int PlayerLevel = 0;
+        public int PlayerXP = 0;
+
+    
+    }
+}
