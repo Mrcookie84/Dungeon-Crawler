9900752e915e97cf75f5606a2944fa16f6fdfb57
Milanosaurus64 <146529520+Milanosaurus64@users.noreply.github.com>
2025-05-15 16:34:18 +0200

    Types de Dégats

--- Dungeon Crawler/Assets/Scripts/Datas/DamageTypesData.cs
+++ Dungeon Crawler/Assets/Scripts/Datas/DamageTypesData.cs
@@ -10,6 +10,8 @@ public class DamageTypesData : ScriptableObject
     {
         Reality,
         Crush,
-        Contact
+        Contact,
+        Degradation,
+        DimensionalWeakness
     }
 }
